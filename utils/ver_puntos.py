e024495dd5d083b4a593a75010adf56c1c020817
Daniela Páez De Miguel <dpaezdemiguel@uade.edu.ar>
2025-10-12 16:30:35 -0300

    Add files via upload
    funcion para ver la cantidad de puntos acumulados

---
+++ utils/ver_puntos.py
@@ -0,0 +1,8 @@
+def ver_puntos(puntos):
+    """
+    Muestra la cantidad actual de puntos acumulados por el usuario.
+    """
+    print(f"\n⭐ Actualmente tienes {puntos} puntos acumulados.")
+    
+
+
